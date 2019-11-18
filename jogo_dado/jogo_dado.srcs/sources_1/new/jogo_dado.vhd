2552ce7f3eed6aa96481380105438409de5da7d3
danilow200 <40957754+danilow200@users.noreply.github.com>
2019-11-18 09:53:46 -0300

    port map atualizado

--- jogo_dado/jogo_dado.srcs/sources_1/new/jogo_dado.vhd
+++ jogo_dado/jogo_dado.srcs/sources_1/new/jogo_dado.vhd
@@ -6,11 +6,13 @@ Port (
     signal clk : in std_logic;
     jogador1 : in std_logic;
     jogador2 : in std_logic;
-    an0, an1, an2, an3, seg : out std_logic
+    an0, an1, an2, an3 : out std_logic;
+    seg : out std_logic_vector(6  downto 0)
 );
 end jogo_dado;
 
 architecture Behavioral of jogo_dado is
+
     component clk_div is
         Port (
             signal clk : in STD_LOGIC;
@@ -39,17 +41,28 @@ architecture Behavioral of jogo_dado is
                 -- colocar todas as declaracoes contidas na entity do componente aqui
        -- );
     --end component;
-    --component decoder is
-        --Port (
-               -- colocar todas as declaracoes contidas na entity do componente aqui
-        --);
-    --end component;
+    component decoder is
+        Port (
+               clk2: IN STD_LOGIC ;
+            val1 : in STD_LOGIC_VECTOR (3 downto 0);
+            val2 : in STD_LOGIC_VECTOR (3 downto 0);
+            seg : out STD_LOGIC_VECTOR (6 downto 0)
+        );
+    end component;
 
     --signal clock : std_logic;
     signal clk1out , clk2out, clk3out : std_logic; -- saidas de clock. todas correspondem com seus numeros no diagrama
     signal loadout1, loadout2 : std_logic; -- sinais de load do num_load
+    signal val1, val2 : STD_LOGIC_VECTOR (3 downto 0);
+    signal segout: STD_LOGIC_VECTOR (6 downto 0);
 
 begin
+    
+    an0 <= clk2out;
+    an3 <= not clk2out;
+    an1 <='1';
+    an2 <= '1';
+    
     c1 : clk_div port map(
         clk => clk,
         clk1 => clk1out,
@@ -66,6 +79,13 @@ begin
     );
     --c2 : num_gen port map();
     --c4 : referee port map();
-    --c5: decoder port map();
+    c5: decoder port map(
+        clk2 => clk2out,
+        val1 => val1,
+        val2 => val2, 
+        seg =>segout
+    );
+    
+    seg <= segout;
 
 end Behavioral;
